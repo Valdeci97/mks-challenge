9fcd65cfd65daf248bbf2e4664a4bf3b6b83e81f
valdeci <moura97.valdeci@gmail.com>
2022-08-23 20:57:20 -0300

    build(auth): add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MovieModule } from './app/movie/movie.module';
 import { UsersModule } from './app/users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -25,6 +26,7 @@ import { UsersModule } from './app/users/users.module';
     }),
     MovieModule,
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
