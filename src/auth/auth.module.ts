73d4f03405e69effe0aaa550b5e7c4d44012b12c
valdeci <moura97.valdeci@gmail.com>
2022-08-23 20:50:21 -0300

    feat(auth): create auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/app/users/users.module';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { UsersService } from 'src/app/users/users.service';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      privateKey: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+})
+export class AuthModule {}
