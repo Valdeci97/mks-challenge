c1eb578ee740e430b108cfd38b82b3da75567226
valdeci <moura97.valdeci@gmail.com>
2022-08-23 22:57:23 -0300

    build(app): set redis configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import 'dotenv/config';
-import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import redisStore from 'cache-manager-redis-store';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MovieModule } from './app/movie/movie.module';
@@ -24,11 +26,21 @@ import { AuthModule } from './auth/auth.module';
       synchronize: true, // Não utilizar isso em produção para evitar perder dados no banco de dados.
       logging: true,
     }),
+    CacheModule.register({
+      store: redisStore,
+      socket: {
+        host: process.env.DB_CACHE_HOST,
+        port: process.env.DB_CACHE_PORT,
+      },
+    }),
     MovieModule,
     UsersModule,
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_INTERCEPTOR, useClass: CacheInterceptor },
+  ],
 })
 export class AppModule {}
