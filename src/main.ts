f5a7fc6ac30be135732f5fd8fc23335bcd1af1ce
valdeci <moura97.valdeci@gmail.com>
2022-08-24 14:30:56 -0300

    feat(swagger): set swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const config = new DocumentBuilder()
+    .setTitle('Movies Api')
+    .setDescription('Simple CRUD rest api developed to mks challenge')
+    .setVersion('0.0.1')
+    .addTag('Movies')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
